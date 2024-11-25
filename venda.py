251606d995534e2173f6364f2293c23d19b8c8dc
adinaifrn11 <162191818+adinaifrn11@users.noreply.github.com>
2024-11-25 23:12:31 +0000

    att 10 atualizada

--- venda.py
+++ venda.py
@@ -33,8 +33,8 @@
             for produto in self.__produtos:
                 print(f"Nome: {produto.get_nome()}, Preço: R${produto.get_preco():.2f}, Quantidade: {produto.get_quantidade()}")
     
-    def salvar_em_json(self, caminho_arquivo):
-        with open(caminho_arquivo, 'w') as f:
+    def salvar_em_json(self, salvar_em_json):
+        with open(salvar_em_json, 'w') as f:
             json.dump({
                 "dataVenda": self.__dataVenda,
                 "produtos": [{"nome": p.get_nome(), 
@@ -43,8 +43,8 @@
                 "total": self.__total
             }, f, ensure_ascii=False, indent=4)
     
-    def carregar_de_json(self, caminho_arquivo):
-        with open(caminho_arquivo, 'r') as f:
+    def carregar_de_json(self,carregar_de_json ):
+        with open(carregar_de_json, 'r') as f:
             dados = json.load(f)
             self.__dataVenda = dados["dataVenda"]
             self.__produtos = [Produto(p["nome"], p["preco"], p["quantidade"]) for p in dados["produtos"]]
