5a4ff7a55c9e4ff9047e8edb2212e0d784383bd6
adinaifrn11 <162191818+adinaifrn11@users.noreply.github.com>
2024-11-25 23:05:09 +0000

    att 10

--- venda.py
+++ venda.py
@@ -1,35 +1,51 @@
-from produto import Produto
-from produto import produto
-
-class Venda:
-    def __init__(self, dataVenda):
-        self.__produtos = []  
+  def __init__(self, dataVenda):
+        self.__produtos = []
         self.__dataVenda = dataVenda
         self.__total = 0.0
-
-  
+    
     def get_produtos(self):
         return self.__produtos
-
+    
     def get_dataVenda(self):
         return self.__dataVenda
-
+    
     def get_total(self):
         return self.__total
-
-
+    
     def set_dataVenda(self, dataVenda):
         self.__dataVenda = dataVenda
-
-    def set_total(self, total):
+    
+    def calcularTotal(self):
+        total = 0.0
+        for produto in self.__produtos:
+            total += produto.get_preco() * produto.get_quantidade()
         self.__total = total
-
-    def adicionar_produto(self, produto: Produto):
-        self.__produtos.produto 
-
-    def remover_produto(self, prduto: produto):
-        self.__produtos.produto
-
-    def calcular_total(self,produto: produto):
-        self.__produtos = ("get_preço * get_quantidade = produtos")
-
+        return total
+    
+    def salvarProduto(self, produto):
+        self.__produtos.append(produto)
+    
+    def listarProdutos(self):
+        if not self.__produtos:
+            print("Nenhum produto na venda.")
+        else:
+            print(f"\nProdutos na Venda do dia {self.__dataVenda}:")
+            for produto in self.__produtos:
+                print(f"Nome: {produto.get_nome()}, Preço: R${produto.get_preco():.2f}, Quantidade: {produto.get_quantidade()}")
+    
+    def salvar_em_json(self, caminho_arquivo):
+        with open(caminho_arquivo, 'w') as f:
+            json.dump({
+                "dataVenda": self.__dataVenda,
+                "produtos": [{"nome": p.get_nome(), 
+                              "preco": p.get_preco(), 
+                              "quantidade": p.get_quantidade()} for p in self.__produtos],
+                "total": self.__total
+            }, f, ensure_ascii=False, indent=4)
+    
+    def carregar_de_json(self, caminho_arquivo):
+        with open(caminho_arquivo, 'r') as f:
+            dados = json.load(f)
+            self.__dataVenda = dados["dataVenda"]
+            self.__produtos = [Produto(p["nome"], p["preco"], p["quantidade"]) for p in dados["produtos"]]
+            self.__total = dados["total"]
