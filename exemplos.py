b021075b9ac104212e5097018280179583ecddde
Adina Lourrane Silva dos Santos <lourrane.a@academico.ifrn.edu.br>
2024-12-03 13:53:10 -0300

    exemplo 2

--- exemplos.py
+++ exemplos.py
@@ -6,8 +6,6 @@ info = "Esse texto sera/nexibido no rotulo/nem varias linhas."
 rotulo = Label(janela, text = info, justify="left")
 rotulo.grid(row=0, column=0)
 
-info2 = """"asssim tambem é possivel ter varias linhas"""
-
-rotulo2 = Label(janela,text= info2,justify= "right")
-rotulo2.grid(row=2,column=0)
-
+logo = PhotoImage(file="logo.png")
+rotulo2 = Label(janela, image= logo)
+rotulo2.grid(row=0,column=1)
